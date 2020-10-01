5504f1aae646360bd23cce2d26e5d2d23d3409f6
3balljuggler <3balljuggling@gmail.com>
2020-10-01 11:34:48 +0900

    [対応済]商品購入機能の修正依頼

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,14 @@
 class OrdersController < ApplicationController
+
+  before_action :set_order, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     return redirect_to root_path if !@item.order.nil? || user_signed_in? == false || current_user.id == @item.user_id
 
     @order_shipping = OrderShipping.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_shipping_params)
     if @order_shipping.valid?
       pay_item
@@ -32,4 +33,9 @@ class OrdersController < ApplicationController
       currency: 'jpy'
     )
   end
+
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
+
 end
