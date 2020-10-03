6849e09c2b8d646ff8d1d04ae30a5f74bcc31ecf
3balljuggler <3balljuggling@gmail.com>
2020-10-03 12:07:08 +0900

    細かい修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,7 +49,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to root_path unless user_signed_in?
   end
 
   def set_item
